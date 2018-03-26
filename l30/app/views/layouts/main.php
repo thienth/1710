7624b9998564b2c29c8f3b05d45b01cf52fbc543
thienth32 <thienth32@gmail.com>
2018-03-26 20:41:01 +0700

    update

---
+++ l30/app/views/layouts/main.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title><?= $title ?></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<div>
+		<h3>Header</h3>
+	</div>
+
+	<main>
+		<?php
+		include_once $view; 
+
+		 ?>
+	</main>
+
+
+	<div>
+		<h3>Footer</h3>
+	</div>
+</body>
+</html>
